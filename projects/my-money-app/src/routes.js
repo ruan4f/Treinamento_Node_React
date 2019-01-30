3df12da7285719499ad250024cc926171d7c446c
Ruan <ruan4f@gmail.com>
2019-01-30 21:08:50 -0200

    Adjusts in layout

--- projects/my-money-app/src/routes.js
+++ projects/my-money-app/src/routes.js
@@ -4,8 +4,6 @@ import DefaultLayout from './containers/DefaultLayout';
 const Dashboard = React.lazy(() => import('./views/Dashboard'));
 const Colors = React.lazy(() => import('./views/Theme/Colors'));
 const Typography = React.lazy(() => import('./views/Theme/Typography'));
-const Users = React.lazy(() => import('./views/Users/Users'));
-const User = React.lazy(() => import('./views/Users/User'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
@@ -14,8 +12,6 @@ const routes = [
   { path: '/theme', exact: true, name: 'Theme', component: Colors },
   { path: '/theme/colors', name: 'Colors', component: Colors },
   { path: '/theme/typography', name: 'Typography', component: Typography },
-  { path: '/users', exact: true, name: 'Users', component: Users },
-  { path: '/users/:id', exact: true, name: 'User Details', component: User },
 ];
 
 export default routes;
--- projects/my-money-app/src/views/index.js
+++ projects/my-money-app/src/views/index.js
@@ -18,10 +18,8 @@ import {
   Tooltips,
 } from './Base';
 
-import { ButtonDropdowns, ButtonGroups, Buttons, BrandButtons } from './Buttons';
+
 import Dashboard from './Dashboard';
-import { CoreUIIcons, Flags, FontAwesome, SimpleLineIcons } from './Icons';
-import { Alerts, Badges, Modals } from './Notifications';
 import { Login, Page404, Page500, Register } from './Pages';
 import { Colors, Typography } from './Theme';
 
