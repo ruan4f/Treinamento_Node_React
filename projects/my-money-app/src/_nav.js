67d3f7ed43eb747b08437aab56cfaeb80fcc1bd6
Ruan <ruan4f@gmail.com>
2019-01-31 20:47:11 -0200

    Adjusts in layout

--- projects/my-money-app/src/_nav.js
+++ projects/my-money-app/src/_nav.js
@@ -7,22 +7,12 @@ export default {
     },
     {
       title: true,
-      name: 'Theme',
+      name: 'My Money',
       wrapper: {            // optional wrapper object
         element: '',        // required valid HTML5 element tag
         attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
       },
       class: ''             // optional class names space delimited list for title item ex: "text-center"
-    },
-    {
-      name: 'Colors',
-      url: '/theme/colors',
-      icon: 'icon-drop',
-    },
-    {
-      name: 'Typography',
-      url: '/theme/typography',
-      icon: 'icon-pencil',
     },
   ],
 };
