8682d4ef09d2d732889dd5319518f0d8b375b70f
Ruan <ruan4f@gmail.com>
2019-02-10 17:44:51 -0200

    Added new component

---
+++ projects/examples/src/components/SelectSearch.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class SelectSearch extends Component {
+  
+  
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
