0f717d80e861592c4fa145b16169ded083f51119
Ruan Ferreira <ruan4f@gmail.com>
2019-02-12 13:30:42 -0200

    Changes dashboard

--- projects/my-money-app/src/views/Dashboard/Dashboard.js
+++ projects/my-money-app/src/views/Dashboard/Dashboard.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
-import { Row, Container } from 'reactstrap';
+import { Row, Container, Col } from 'reactstrap';
+import Widget03 from '../../components/Widget03';
+
+import api from '../../services/api';
 
 class DashBoard extends Component {
+
+  state = {
+    credito: 0,
+    debito: 0,
+    valorConsolidado: 0
+  }
+
+  componentDidMount() {
+    this.listarSumario();
+  }
+
+  listarSumario = () => {
+    api.get('/cicloPagamentos/summary')
+      .then(res => {
+        const { credito, debito } = this.state;
+        console.log(res.data);
+
+        this.setState({
+          credito,
+          debito,
+          valorConsolidado: credito - debito
+        });
+      })
+      .catch(error => {
+
+      });
+  }
+
   render() {
     return (
       <div className="animated fadeIn">
         <Container>
           <Row className="justify-content-center">
-            <h1>Bem Vindo</h1>
+
+            <Col xs="6" sm="6" lg="3">
+              <Widget03 dataBox={() => ({
+                variant: 'twitter',
+                credito: this.state.credito,
+                debito: this.state.debito,
+                consolidado: this.state.valorConsolidado
+              })} >
+
+              </Widget03>
+            </Col>
           </Row>
         </Container>
       </div>
