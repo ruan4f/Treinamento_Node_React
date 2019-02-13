6c17961266f2ea8d6973ae2abb29b81c3a0c31aa
Ruan <ruan4f@gmail.com>
2019-02-12 22:37:24 -0200

    Changes

--- projects/examples/src/components/AutoComplete.js
+++ projects/examples/src/components/AutoComplete.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Autocomplete extends Component {
+  static propTypes = {
+    options: PropTypes.instanceOf(Array).isRequired
+  };
+  state = {
+    activeOption: 0,
+    filteredOptions: [],
+    showOptions: false,
+    userInput: ''
+  };
   render() {
+    const {
+      onChange,
+      onChange,
+      onKeyDown,
+      userInput,
+      state: { activeOption, filteredOptions, showOptions, userInput }
+    } = this;
+    let optionList;
     return (
       <React.Fragment>
         <div className="search">
-          <input type="text" className="search-box" />
+          <input
+            type="text"
+            className="search-box"
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            value={userInput}
+          />
           <input type="submit" value="" className="search-btn" />
+          {optionList}
         </div>
       </React.Fragment>
     );
