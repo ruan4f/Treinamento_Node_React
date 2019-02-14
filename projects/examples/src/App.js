cb1d961efcb05cd000a4aac64d9bc956b9330839
Ruan Ferreira <ruan4f@gmail.com>
2019-02-14 08:33:47 -0200

    Adjusts

--- projects/examples/src/App.js
+++ projects/examples/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Autocomplete from './components/Autocomplete';
+import AutoComplete from './components/AutoComplete';
 
 const App = () => {
   return (
     <div className="App">
-      <Autocomplete
+      <AutoComplete
         options={[
           'Papaya',
           'Persimmon',
