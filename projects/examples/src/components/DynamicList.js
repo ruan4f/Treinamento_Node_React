236bd8cee7f8f1b610663756dbb8868f37a6d51c
Ruan Ferreira <ruan4f@gmail.com>
2019-02-14 08:50:56 -0200

    New component

---
+++ projects/examples/src/components/DynamicList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class DynamicList extends Component {
+  render() {
+    return (
+        <div>
+            
+        </div>
+    );
+  }
+}
