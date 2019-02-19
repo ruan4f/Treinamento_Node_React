6324f9e5ba638949cbc94decffb260f0e0e70b41
Ruan <ruan4f@gmail.com>
2019-02-18 22:52:06 -0300

    Updates CSS

--- projects/examples/src/views/Home/Home.js
+++ projects/examples/src/views/Home/Home.js
@@ -7,10 +7,10 @@ import './home.css';
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <Link className="link-example link-example-primary" to="/" >Home</Link>
         <Link className="link-example link-example-primary" to="/search-select">Search Select</Link>
-      </div>
+      </React.Fragment>
     );
   }
 }
