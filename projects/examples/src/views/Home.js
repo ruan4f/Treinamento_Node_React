7708b308c4b70bcc0aeec26255379edf395deaf9
Ruan <ruan4f@gmail.com>
2019-02-18 22:22:06 -0300

    Created a new component

---
+++ projects/examples/src/views/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
