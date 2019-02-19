cfbaedaff3868d2aff1f0ee369a86f9d829e84f5
Ruan <ruan4f@gmail.com>
2019-02-18 22:21:24 -0300

    Add new lib

--- projects/examples/src/App.js
+++ projects/examples/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 import AutoComplete from './components/AutoComplete';
 
 const App = () => {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          
+        </Switch>
+      </Router>
+      
       <AutoComplete
         options={[
           'Papaya',
