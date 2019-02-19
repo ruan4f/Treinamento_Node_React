51d8d42ae25401ae6e32a00012fc488cea103186
Ruan <ruan4f@gmail.com>
2019-02-18 22:31:21 -0300

    New links

--- projects/examples/src/views/Home/Home.js
+++ projects/examples/src/views/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 // import { Container } from './styles';
 
 import './home.css';
@@ -8,7 +8,8 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-
+        <Link to="/">Home</Link>
+        <Link to="/search-select">Search Select</Link>
       </div>
     );
   }
