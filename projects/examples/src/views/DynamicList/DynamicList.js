9926e3264f1e0d457c0e7d2e870c8c6c732b86f8
Ruan <ruan4f@gmail.com>
2019-02-19 23:30:36 -0300

    include file CSS

--- projects/examples/src/views/DynamicList/DynamicList.js
+++ projects/examples/src/views/DynamicList/DynamicList.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 // import { Container } from './styles';
 
+import './dynamicList.css';
+
 export default class DynamicList extends Component {
   render() {
     return (
       <div>
-        
+
       </div>
     );
   }
